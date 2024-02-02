0c3120b9398aa820a100ff26ef02969a7a224dfd
Denis Tumakov <v1291l@Deniss-MacBook-Air.local>
2024-02-02 17:22:53 +0200

    refactor echo command

--- app/server/message_parser.cr
+++ app/server/message_parser.cr
@@ -1,15 +1,26 @@
-def message_parser(command)
+def message_parser(command_parts)
+  command = command_parts.join("\r\n")
   puts "COMMAND: #{command}"
-
-  # Split the command by line breaks
   parts = command.split("\r\n")
 
   # Check for the PING command
-  if parts.any? { |part| part.upcase == "PING" }
+  if parts.size >= 3 && parts[0] == "*1" && parts[1] == "$4" && parts[2].upcase == "PING"
     return "+PONG"
   end
 
-  # ECHO command handling remains unchanged
+  # Check if it's an ECHO command
+  if parts.size >= 5 && parts[0] == "*2" && parts[1] == "$4" && parts[2].upcase == "ECHO" && parts[3].starts_with?("$")
+    message_length = parts[3][1..].to_i
+    message_to_echo = parts[4]
+
+    if message_to_echo.bytesize == message_length
+      response = "$#{message_length}\r\n#{message_to_echo}\r\n"
+      return response
+    else
+      puts "Invalid ECHO command length"
+      return nil
+    end
+  end
 
   puts "NOT SUPPORTED: #{command}"
   return nil # Return nil if the command does not match any supported command
--- app/server/server.cr
+++ app/server/server.cr
@@ -1,5 +1,5 @@
 require "socket"
-# require "./message_parser"
+require "./message_parser"
 
 # Ensure that the program terminates on SIGTERM, https://github.com/crystal-lang/crystal/issues/8687
 Signal::TERM.trap { exit }
@@ -51,35 +51,6 @@ class Server
       end
     end
   end
-
-  def message_parser(command_parts)
-    command = command_parts.join("\r\n")
-    puts "COMMAND: #{command}"
-    parts = command.split("\r\n")
-
-    # Check for the PING command
-    if parts.size >= 3 && parts[0] == "*1" && parts[1] == "$4" && parts[2].upcase == "PING"
-      return "+PONG"
-    end
-
-    # Check if it's an ECHO command
-  if parts.size >= 5 && parts[0] == "*2" && parts[1] == "$4" && parts[2].upcase == "ECHO" && parts[3].starts_with?("$")
-    message_length = parts[3][1..].to_i
-    message_to_echo = parts[4]
-
-    if message_to_echo.bytesize == message_length
-      response = "$#{message_length}\r\n#{message_to_echo}\r\n"
-      return response
-    else
-      puts "Invalid ECHO command length"
-      return nil
-    end
-  end
-
-    puts "NOT SUPPORTED: #{command}"
-    return nil # Return nil if the command does not match any supported command
-  end
-
 end
 
 Server.new.start
