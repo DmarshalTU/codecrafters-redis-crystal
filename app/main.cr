1e49fc21b55661dad8b069ffa774e2e819163a3c
Denis Tumakov <v1291l@Deniss-MacBook-Air.local>
2024-02-02 14:41:43 +0200

    stage 2

--- app/main.cr
+++ app/main.cr
@@ -8,9 +8,9 @@ class YourRedisServer
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     puts("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
     server = TCPServer.new("0.0.0.0", 6379, reuse_port: true)
-    client = server.accept?
+    return if (client = server.accept?).nil?
+    client << "+PONG\r\n"
   end
 end
 
